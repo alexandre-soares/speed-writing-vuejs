fa066b2eb2968cfdece2998a0af51f872ba373b4
Alexandre Soares <alexandresoares@Alexandres-MacBook-Pro.local>
2021-01-21 12:20:21 +0200

    adding html and css

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,29 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  min-height: 100vh;
+  font-family: "Roboto", sans-serif;
+}
+
+html {
+  font-size: 62.5%;
 }
 </style>
--- src/components/SpeedWriting.vue
+++ src/components/SpeedWriting.vue
@@ -1,9 +1,58 @@
 <template>
-  <div class="speed-writing"></div>
+  <div class="speed-writing">
+    <h1>Speed Writing Test</h1>
+    <div class="timer">0</div>
+    <div class="container">
+      <div class="quote-display">Quote</div>
+      <textarea class="quote-input" autofocus>Quote</textarea>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {}
+};
 </script>
 
-<style></style>
+<style>
+h1 {
+  letter-spacing: 2px;
+  margin-bottom: 4rem;
+  font-size: 4rem;
+}
+
+.container {
+  height: 70vh;
+  width: 50vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+}
+
+.timer {
+  font-size: 4rem;
+}
+
+.quote-display {
+  font-size: 3rem;
+}
+
+.quote-input {
+  border: 2px solid black;
+  border-radius: 1rem;
+  width: 100%;
+  height: 15rem;
+  padding: 2rem;
+  font-size: 1.9rem;
+  letter-spacing: 1px;
+  line-height: 1.6;
+  margin: auto;
+  resize: none;
+}
+
+.quote-input:focus {
+  outline: none;
+}
+</style>
