b56f051472fd3474ca55b88755472f3bf800ee2e
Alexandre Soares <alexandresoares@Alexandres-MacBook-Pro.local>
2021-01-21 12:05:01 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Speed-writing />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import SpeedWriting from "./components/SpeedWriting.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    SpeedWriting
   }
 };
 </script>
---
+++ src/components/SpeedWriting.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="speed-writing"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
